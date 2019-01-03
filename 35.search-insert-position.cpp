605591940ae417472c70e61c53b8a340c28b364f
huangankun <646741916@qq.com>
2019-01-03 10:20:50 +0800

    占坑

---
+++ 35.search-insert-position.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (40.20%)
+ * Total Accepted:    337.7K
+ * Total Submissions: 840K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ * 
+ * You may assume no duplicates in the array.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ * 
+ * 
+ */
+class Solution 
+{
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int count = 0;
+        for(auto iter = nums.begin(); iter != nums.end(); iter++)
+        {
+            if(*iter < target)
+                count++;
+        }
+        return count;
+    }
+};
