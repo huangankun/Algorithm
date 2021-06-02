7bad13e5c6092a02915991e54bc28f8426dc7b0a
安坤 黄 <ankun.ace.huang@gmail.com>
2021-06-02 14:37:55 +0800

    两数之和Go解法

---
+++ .leetcode/1.两数之和.go
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	//暴力枚举，时间复杂度为o(n^2)，空间复杂度为o(1)
+	// for i := 0; i < len(nums); i++ {
+	// 	var res []int
+	// 	res = append(res, i)
+	// 	for j := i + 1; j < len(nums); j++ {
+	// 		if nums[i]+nums[j] == target {
+	// 			res = append(res, j)
+	// 			return res
+	// 		}
+	// 	}
+	// }
+
+	//时间复杂度为o(n),空间复杂度为o(n)
+	// m := map[int]int{}
+	// for i := 0; i < len(nums); i++ {
+	// 	if p, ok := m[target-nums[i]]; ok {
+	// 		return []int{p, i}
+	// 	}
+	// 	m[nums[i]] = i
+	// }
+	// return nil
+
+	m := map[int]int{}
+	for i, x := range nums {
+		if p, ok := m[target-x]; ok {
+			return []int{p, i}
+		}
+		m[x] = i
+	}
+	return nil
+}
+
+// @lc code=end
+
