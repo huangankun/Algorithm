9ac0036c3cb0469a9bfc370d21175e08e3fe9392
安坤 黄 <ankun.ace.huang@gmail.com>
2021-06-02 15:10:56 +0800

    两数想加

---
+++ .leetcode/2.两数相加.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=golang
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) (head *ListNode) {
+
+	//时间复杂度为o(max(m,n))
+	var tail *ListNode
+	var sum, carry int
+	for l1 != nil || l2 != nil {
+		var n1, n2 int
+		if l1 != nil {
+			n1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			n2 = l2.Val
+			l2 = l2.Next
+		}
+		sum = n1 + n2 + carry
+		sum, carry = sum%10, sum/10
+		if head == nil {
+			head = &ListNode{Val: sum}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: sum}
+			tail = tail.Next
+		}
+	}
+	if carry > 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+	return
+}
+
+// @lc code=end
+
