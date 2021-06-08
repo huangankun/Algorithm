7f1c08e033ccbcabb281b785c2775863081c3aa3
安坤 黄 <ankun.ace.huang@gmail.com>
2021-06-08 17:16:38 +0800

    实现 strStr()

---
+++ .leetcode/28.实现-str-str.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=golang
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	for i := 0; i < len(haystack)-len(needle)+1; i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+// @lc code=end
+
