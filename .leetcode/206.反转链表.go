1a74c2fabbbd6f0ebdfd5a6f624db71875d0353b
安坤 黄 <ankun.ace.huang@gmail.com>
2021-06-13 16:38:14 +0800

    添加时间复杂度和空间复杂度

--- .leetcode/206.反转链表.go
+++ .leetcode/206.反转链表.go
@@ -13,6 +13,7 @@
  * }
  */
 func reverseList(head *ListNode) *ListNode {
+	//时间复杂度On，空间复杂度O1
 	// //这个地方不能够使用&ListNode{}结构，会自动初始化
 	// var reverse *ListNode
 	// curHead := head
@@ -25,6 +26,8 @@ func reverseList(head *ListNode) *ListNode {
 	// return reverse
 
 	//递归
+	//时间复杂度On
+	//空间复杂度On
 	if head == nil || head.Next == nil {
 		return head
 	}
