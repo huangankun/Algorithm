f3b026cbcd3366a7977551c88c06e7ffbad540a6
黄安坤 <ankun.ace.huang@gmail.com>
2021-06-16 22:04:48 +0800

    [141] 环形链表

---
+++ .leetcode/141.环形链表.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=golang
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head.Next
+	for fast != slow {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
+
+// @lc code=end
+
